03ee4b48fc17b40e48cce71a9ef2e89d9e0a376e
jackdaw213 <103126046+jackdaw213@users.noreply.github.com>
2024-05-16 15:41:52 +1000

    Finished test.py

--- test.py
+++ test.py
@@ -16,23 +16,30 @@ parser.add_argument('-c', '--content', type=str,
 parser.add_argument('-s', '--style', type=str,
                     help='Path to the style image')
 parser.add_argument('-m', '--model', type=str,
+                    default="model/artistic.pt",
                     help='Path to the model')
 
 args = parser.parse_args()
 
-model = torch.jit.load('model/model.pt')
+model = torch.jit.load(args.model)
 model.eval()
 
-content = F.to_tensor(Image.open(args.content).convert("RGB"))
+content = Image.open(args.content).convert("RGB")
 content = utils.norm_pil(content).unsqueeze(dim=0).float()
 
-style = F.to_tensor(Image.open(args.style).convert("RGB"))
+style = Image.open(args.style).convert("RGB")
 style = utils.norm_pil(style).unsqueeze(dim=0).float()
 
 with torch.no_grad():
     output = model(content, style)
 
 output = utils.denorm(output.squeeze())
+F.to_pil_image(output).save("output.jpg")
 
-plt.imshow(output.permute(1, 2, 0))
+image = utils.paste_concat_images(utils.denorm(content.squeeze()), 
+                                  utils.denorm(style.squeeze()), 
+                                  output.squeeze())
+image.save("comp.jpg")
+
+plt.imshow(image)
 plt.show()
